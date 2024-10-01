1a6946763e4f451c67f051081c66da0a6f98f9d7
Michele J <90045067+Jasmim-mii@users.noreply.github.com>
2024-10-01 16:04:34 -0300

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '......key......'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
