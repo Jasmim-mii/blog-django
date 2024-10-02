1b60e655946bcc00e183709e791104617cad6ad1
Michele J <90045067+Jasmim-mii@users.noreply.github.com>
2024-10-01 21:52:08 -0300

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5ak1&c1=a$c2!%f@!5_j8wl801zs@1&5f)x@lpvb6cqe*c29#w''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
